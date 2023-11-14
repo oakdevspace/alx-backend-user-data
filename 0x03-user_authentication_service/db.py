c5540fd10854fd0964868a3562d1bf14eb8384d4
Kabiru Umar <111059385+oakdevspace@users.noreply.github.com>
2023-11-14 02:47:26 +0100

    push files

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""Object realational mapper database """
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from typing import TypeVar
+from user import Base, User
+
+
+class DB:
+    """ Object relational mapper database """
+
+    def __init__(self):
+        """class initializer """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self):
+        """ Session Getter Method """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ Adds user to database and return the object added"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Finds user and return the first user
+        found in the users table based onkwargs
+        """
+        if not kwargs:
+            raise InvalidRequestError
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
+
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """ Update user information in user table"""
+        user = self.find_user_by(id=user_id)
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise ValueError
+
+        for key, value in kwargs.items():
+            setattr(user, key, value)
+
+        self._session.commit()
